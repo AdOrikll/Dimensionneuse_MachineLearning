24a3edcc88d490ca587d8977499902951ce22867
yopox <yopoxdev@gmail.com>
2020-12-07 23:49:54 +0100

    Fix sending & photo analysis

--- TM_Dimn.js
+++ TM_Dimn.js
@@ -1,11 +1,9 @@
-const TeachableMachine = require("@sashido/teachablemachine-node");
 
-const model = new TeachableMachine({
-    modelUrl: "https://teachablemachine.withgoogle.com/models/u3cWdHHG1/"
-});
 
 exports.classify = async function(url) {
-    return await model.classify({
+    const results = await model.classify({
         imageUrl: url,
     });
+    console.log(results)
+    return results
 }
--- server.js
+++ server.js
@@ -2,19 +2,43 @@ const express = require('express');
 const app = express()
 const port = 3000
 const ip = require('ip');
-const teachablemachine = require('./TM_Dimn');
+const TeachableMachine = require("@sashido/teachablemachine-node");
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({
+    storage: multer.diskStorage(
+        {
+            destination: './uploads/',
+            filename: function (req, file, cb) {
+                cb(null, file.originalname);
+            }
+        }
+    )
+});
 
 app.listen(port, () => {
-    console.log(`Serveur lancé:\thttp://localhost:${port}`)
+    console.log(`Serveur lancé:\t\thttp://localhost:${port}`)
     console.log(`Accès à distance:\thttp://${ip.address()}:${port}`)
 })
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 
 // Site
 app.use(express.static('website'))
 
 // POST endpoint
-app.post('/image', function (req, res) {
-    console.log(req);
-    return teachablemachine.classify(req.url);
+app.post('/image', upload.single('image'), function (req, res, next) {
+    console.log(req.file);
+
+    const model = new TeachableMachine({
+        modelUrl: "https://teachablemachine.withgoogle.com/models/u3cWdHHG1/"
+    });
+
+    const url = __dirname + '/' + req.file.path
+    const results = model.classify({imageUrl: url})
+        .then(r => {
+            console.log(r)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
